48c08a2923caeb96abac087c8b963db03b011710
warungerik <officialerik28@gmail.com>
2025-07-03 04:29:58 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+// SCRIPT BY WARUNGERIK
+// ORDER ATAU TANYA TANYA BISA KE 
+// WHATSAPP : 085183129647
+// TELEGRAM : @WARUNGERIK
+export default {
+    // --- PENGATURAN PANEL ---
+    domain: 'https://free.erikcode.biz.id', // URL PANEL ANDA
+    apikey: 'ptla_GCHxt6jNTaxGjzMIlWxkdCjfUcy1BosHVtJvTQhCgCj', // API KEY APPLICATION (PTLA) ANDA
+    eggsnya: '15',                          // ID EGG NODEJS (biasanya 15)
+    location: '1',                        // ID LOKASI (biasanya 1)
+
+    // --- PENGATURAN BOT TELEGRAM ---
+    botToken: '7374804235:AAGECFOKbpddmy39oXjnm3c3c324Vsu8v9E', // Token dari @BotFather
+    adminNumber: '7885087085', // User ID Telegram Anda (berupa angka)
+    startupNotificationNumber: '7885087085', // User ID untuk notifikasi startup
+    
+    prefix: '.',
+    botName: 'PRIME BOT FREE PANEL',
+    ownerName: 'WARUNGERIK',
+    defaultBotMode: 'public', //'self' atau 'public'
+    botVersion: 'v1.0.0-telegram',
+};
